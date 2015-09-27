b8b4a855285b1926e4c12da15eb62204a7374155
tmacker <acker.tim@gmail.com>
2015-09-27 12:56:33 -0700

    Revert "Added code"
    This reverts commit e8fd3a1f19ffaa0a57e1611587857a3b3fcad021.

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,15 @@
-##  This function creates a special "matrix" object that can cache its inverse.
-## sets the value of the matrix
-## gets the value of the matrix
-## sets the value of the inverse
-## gets the value of the inverse
+## Put comments here that give an overall description of what your
+## functions do
 
-## This caches the matrix
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setinv <- function(inverse) m <<- inverse
-  getinv <- function() m
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)
+
 }
 
 
-## this solves the inverse of the matrix, checking to see if it is cached first
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  m <- x$getinv()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinv(m)
-  m
+        ## Return a matrix that is the inverse of 'x'
 }
-  ## Return a matrix that is the inverse of 'x'
